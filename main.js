d9a60317f74cf3a4724418e04a5a191ed85c80c9
Jamie Binns <jamie.m.binns@gmail.com>
2015-06-22 17:23:21 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,35 @@
+//Asking for the number of victims
+
+var userVictims = prompt("How many disaster victims would you like to submit? Please enter a number, ex. 3, 6, 9.");
+
+confirm("Ok, you will be prompted to enter info for each victim. It'll be a bit redundant, so just be prepared with each victim's name, phone number, and street. ")
+
+victimArray = [];
+
+for(var i = 0; i < userVictims; i++){
+	var victimObject = {};
+	victimObject.victimName = prompt("What is the victim's name?");
+	victimObject.victimPhone = prompt("What is the victim's phone number?");
+	victimObject.victimStreet = prompt("What is the victim's street?");
+	victimArray.push(victimObject);
+}
+
+// Asking for the number of volunteers
+
+var userVolunteers = prompt("How many disaster volunteers would you like to submit? Please enter a number, ex. 3, 6, 9.");
+
+confirm("Ok, you will be prompted to enter info for each volunteer. It'll be a bit redundant, so just be prepared with each victim's name, phone number, and street. ")
+
+volunteerArray = [];
+
+for(var i = 0; i < userVolunteers; i++){
+	var volunteerObject = {};
+	volunteerObject.volunteerName = prompt("What is the volunteers's name?");
+	volunteerObject.volunteerPhone = prompt("What is the volunteers's phone number?");
+	volunteerObject.volunteerStreet = prompt("What is the volunteers's street?");
+	volunteerArray.push(volunteerObject);
+}
+
+console.log(victimArray[1]);
+
+// alert("We've got a ton of people who need help and a ton of people willing to help.\nFirst of all, the victims:\nThere are " + userVictims + " victims.\nTheir info:\n" + victimArray[1] + victimArray[2]);
